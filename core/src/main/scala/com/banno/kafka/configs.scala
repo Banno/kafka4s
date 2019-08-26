2bde57f507fb0eb994419d567abe77f44508403b
gummadi <gummadi.sreenivas@banno.com>
2019-08-26 17:18:33 -0400

    [WIP] proposed Config base class...

--- core/src/main/scala/com/banno/kafka/configs.scala
+++ core/src/main/scala/com/banno/kafka/configs.scala
@@ -25,58 +25,39 @@ import scala.reflect.ClassTag
 
 //TODO other configs... maybe we could auto generate these somehow?
 
+sealed abstract class Config(val _1: String, val _2: AnyRef) extends Product2[String, AnyRef]
+
+object Config {
+  implicit def toTuple(c: Config): (String, AnyRef) = c._1 -> c._2
+}
+
 //helpers to make it simpler to specify configs, is this the best way to do this?
-case class BootstrapServers(bs: String)
-object BootstrapServers {
-  implicit def toConfig(bs: BootstrapServers): (String, AnyRef) =
-    CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG -> bs.bs
-}
+case class BootstrapServers(bs: String) extends Config(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, bs)
 
-object ClientId {
-  def apply(id: String): (String, String) = CommonClientConfigs.CLIENT_ID_CONFIG -> id
-}
+case class ClientId(id: String) extends Config(CommonClientConfigs.CLIENT_ID_CONFIG, id)
 
-case class EnableIdempotence(e: Boolean)
-object EnableIdempotence {
-  implicit def toConfig(ei: EnableIdempotence): (String, AnyRef) =
-    ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG -> ei.e.toString
-}
+case class EnableIdempotence(e: Boolean) extends Config(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, e.toString)
 
-case class CompressionType(c: String)
+case class CompressionType(c: String) extends Config(ProducerConfig.COMPRESSION_TYPE_CONFIG, c)
 object CompressionType {
   val none = CompressionType("none")
   val gzip = CompressionType("gzip")
   val snappy = CompressionType("snappy")
   val lz4 = CompressionType("lz4")
-  implicit def toConfig(ct: CompressionType): (String, AnyRef) =
-    ProducerConfig.COMPRESSION_TYPE_CONFIG -> ct.c
 }
 
-case class GroupId(id: String)
-object GroupId {
-  implicit def toConfig(g: GroupId): (String, AnyRef) = ConsumerConfig.GROUP_ID_CONFIG -> g.id
-}
+case class GroupId(id: String) extends Config(ConsumerConfig.GROUP_ID_CONFIG, id)
 
-case class AutoOffsetReset(aor: String)
+case class AutoOffsetReset(aor: String) extends Config(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, aor)
 object AutoOffsetReset {
   val earliest = AutoOffsetReset("earliest")
   val latest = AutoOffsetReset("latest")
   val none = AutoOffsetReset("none")
-  implicit def toConfig(aor: AutoOffsetReset): (String, AnyRef) =
-    ConsumerConfig.AUTO_OFFSET_RESET_CONFIG -> aor.aor
 }
 
-case class EnableAutoCommit(b: Boolean)
-object EnableAutoCommit {
-  implicit def toConfig(eac: EnableAutoCommit): (String, AnyRef) =
-    ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG -> eac.b.toString
-}
+case class EnableAutoCommit(b: Boolean) extends Config(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, b.toString)
 
-case class KeySerializerClass(c: Class[_])
-object KeySerializerClass {
-  implicit def toConfig(ksc: KeySerializerClass): (String, AnyRef) =
-    ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG -> ksc.c.getName
-}
+case class KeySerializerClass(c: Class[_]) extends Config(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, c.getName)
 
 case class ValueSerializerClass(c: Class[_])
 object ValueSerializerClass {
@@ -84,41 +65,17 @@ object ValueSerializerClass {
     ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG -> vsc.c.getName
 }
 
-case class KeyDeserializerClass(c: Class[_])
-object KeyDeserializerClass {
-  implicit def toConfig(kdc: KeyDeserializerClass): (String, AnyRef) =
-    ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG -> kdc.c.getName
-}
+case class KeyDeserializerClass(c: Class[_]) extends Config(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, c.getName)
 
-case class ValueDeserializerClass(c: Class[_])
-object ValueDeserializerClass {
-  implicit def toConfig(vdc: ValueDeserializerClass): (String, AnyRef) =
-    ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG -> vdc.c.getName
-}
+case class ValueDeserializerClass(c: Class[_]) extends Config(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, c.getName)
 
-case class SchemaRegistryUrl(url: String)
-object SchemaRegistryUrl {
-  implicit def toConfig(sru: SchemaRegistryUrl): (String, AnyRef) =
-    AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG -> sru.url
-}
+case class SchemaRegistryUrl(url: String) extends Config(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, url)
 
-case class MaxSchemasPerSubject(m: Int)
-object MaxSchemasPerSubject {
-  implicit def toConfig(msps: MaxSchemasPerSubject): (String, AnyRef) =
-    AbstractKafkaAvroSerDeConfig.MAX_SCHEMAS_PER_SUBJECT_CONFIG -> msps.m.toString
-}
+case class MaxSchemasPerSubject(m: Int) extends Config(AbstractKafkaAvroSerDeConfig.MAX_SCHEMAS_PER_SUBJECT_CONFIG, m.toString)
 
-case class AutoRegisterSchemas(r: Boolean)
-object AutoRegisterSchemas {
-  implicit def toConfig(ars: AutoRegisterSchemas): (String, AnyRef) =
-    AbstractKafkaAvroSerDeConfig.AUTO_REGISTER_SCHEMAS -> ars.r.toString
-}
+case class AutoRegisterSchemas(r: Boolean) extends Config(AbstractKafkaAvroSerDeConfig.AUTO_REGISTER_SCHEMAS, r.toString)
 
-case class SpecificAvroReader(s: Boolean)
-object SpecificAvroReader {
-  implicit def toConfig(sar: SpecificAvroReader): (String, AnyRef) =
-    KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG -> sar.s.toString
-}
+case class SpecificAvroReader(s: Boolean) extends Config(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, s.toString)
 
 object MetricReporters {
   //TODO need to support multiple reporter types
