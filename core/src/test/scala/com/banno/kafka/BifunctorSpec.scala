45c4810f2b93bf3b9c41231745ad60d1693fe5f8
Keith Pinson <keith.pinson@banno.com>
2021-04-28 10:01:32 -0400

    feat(core): convert `BifunctorSpec` to MUnit

--- core/src/test/scala/com/banno/kafka/BifunctorSpec.scala
+++ core/src/test/scala/com/banno/kafka/BifunctorSpec.scala
@@ -2,12 +2,9 @@ package com.banno.kafka
 
 import cats.laws.discipline.BifunctorTests
 import com.banno.kafka.test._
-import org.scalatest.funsuite.AnyFunSuite
-import org.scalatest.matchers.should.Matchers
-import org.scalatestplus.scalacheck.Checkers
-import org.typelevel.discipline.scalatest.FunSuiteDiscipline
+import munit._
 
-class BifunctorSpec extends AnyFunSuite with Matchers with Checkers with FunSuiteDiscipline {
+class BifunctorSpec extends DisciplineSuite {
   checkAll(
     "ProducerRecordBifunctor",
     BifunctorTests(ProducerRecordBifunctor).bifunctor[Int, Int, Int, String, String, String]
