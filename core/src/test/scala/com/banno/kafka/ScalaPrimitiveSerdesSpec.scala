b4f1ab2e23d9144bd576052f659ebf4bd08bc42b
Keith Pinson <keith.pinson@banno.com>
2021-04-28 11:26:26 -0400

    feat(core): convert `ScalaPrimitiveSerdesSpec` to MUnit

--- core/src/test/scala/com/banno/kafka/ScalaPrimitiveSerdesSpec.scala
+++ core/src/test/scala/com/banno/kafka/ScalaPrimitiveSerdesSpec.scala
@@ -1,20 +1,17 @@
 package com.banno.kafka
 
+import cats.syntax.all._
+import munit._
 import org.apache.kafka.common.serialization._
-import org.scalatest.matchers.should.Matchers
-import org.scalatest.propspec.AnyPropSpec
-import org.scalatestplus.scalacheck._
+import org.scalacheck._
+import org.scalacheck.Prop._
 
-class ScalaPrimitiveSerdesSpec
-    extends AnyPropSpec
-    with ScalaCheckDrivenPropertyChecks
-    with Matchers {
-
+class ScalaPrimitiveSerdesSpec extends ScalaCheckSuite {
   property("Scala Double serde") {
     val s = implicitly[Serializer[Double]]
     val d = implicitly[Deserializer[Double]]
     forAll { x: Double =>
-      d.deserialize("topic", s.serialize("topic", x)) should ===(x)
+      d.deserialize("topic", s.serialize("topic", x)) === x
     }
   }
 
@@ -22,7 +19,7 @@ class ScalaPrimitiveSerdesSpec
     val s = implicitly[Serializer[Float]]
     val d = implicitly[Deserializer[Float]]
     forAll { x: Float =>
-      d.deserialize("topic", s.serialize("topic", x)) should ===(x)
+      d.deserialize("topic", s.serialize("topic", x)) === x
     }
   }
 
@@ -30,7 +27,7 @@ class ScalaPrimitiveSerdesSpec
     val s = implicitly[Serializer[Int]]
     val d = implicitly[Deserializer[Int]]
     forAll { x: Int =>
-      d.deserialize("topic", s.serialize("topic", x)) should ===(x)
+      d.deserialize("topic", s.serialize("topic", x)) === x
     }
   }
 
@@ -38,7 +35,7 @@ class ScalaPrimitiveSerdesSpec
     val s = implicitly[Serializer[Long]]
     val d = implicitly[Deserializer[Long]]
     forAll { x: Long =>
-      d.deserialize("topic", s.serialize("topic", x)) should ===(x)
+      d.deserialize("topic", s.serialize("topic", x)) === x
     }
   }
 
@@ -46,7 +43,7 @@ class ScalaPrimitiveSerdesSpec
     val s = implicitly[Serializer[Short]]
     val d = implicitly[Deserializer[Short]]
     forAll { x: Short =>
-      d.deserialize("topic", s.serialize("topic", x)) should ===(x)
+      d.deserialize("topic", s.serialize("topic", x)) === x
     }
   }
 
