1a0f334ab41927142719e7377eef9cc8325f6443
Keith Pinson <keith.pinson@banno.com>
2021-04-28 12:50:21 -0400

    feat(core): convert `AdminApiSpec` to MUnit

--- core/src/test/scala/com/banno/kafka/AdminApiSpec.scala
+++ core/src/test/scala/com/banno/kafka/AdminApiSpec.scala
@@ -4,35 +4,32 @@ import com.banno.kafka.admin._
 import cats.effect._
 import cats.syntax.all._
 import org.apache.kafka.clients.admin._
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
+import munit._
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 
-class AdminApiSpec extends AnyFlatSpec with Matchers with DockerizedKafka {
-  // TODO switch to MUnit with CE3 integration?
-  import cats.effect.unsafe.implicits.global
-
+class AdminApiSpec extends CatsEffectSuite with DockerizedKafka {
   // Probably don't need to test every single AdminClient operation; this is
   // just a sanity check that it is all wired up properly
-
-  "Admin API" should "create topics idempotently" in {
+  test("Admin API should create topics idempotently") {
     val topicName = randomId
-    def program[F[_]](admin: AdminApi[F])(implicit F: Async[F]) =
+    def program[F[_]: Async](admin: AdminApi[F]) =
       for {
         ltr1 <- admin.listTopics
-        ns1 <- F.delay(ltr1.names().get())
+        ns1 <- Sync[F].delay(ltr1.names().get())
         _ <- admin.createTopicsIdempotent(List(new NewTopic(topicName, 1, 1.toShort)))
         _ <- admin.createTopicsIdempotent(List(new NewTopic(topicName, 1, 1.toShort)))
         _ <- Temporal[F].sleep(1.second) // TODO: Better fix
         ltr2 <- admin.listTopics
-        ns2 <- F.delay(ltr2.names.get())
+        ns2 <- Sync[F].delay(ltr2.names.get())
       } yield (ns1, ns2)
 
-    val (before, after) =
-      AdminApi.resource[IO](BootstrapServers(bootstrapServer)).use(program[IO]).unsafeRunSync()
-    before should not contain (topicName)
-    after should contain(topicName)
+    AdminApi.resource[IO](BootstrapServers(bootstrapServer))
+      .use(program[IO])
+      .map { beforeAndAfter =>
+        val (before, after) = beforeAndAfter
+        !before.contains(topicName) && after.contains(topicName)
+      }.assert
   }
 
 }
