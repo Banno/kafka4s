05ac2c718972a0ca64b567c1f014e937231d819c
Andrew Mohrland <andrew.mohrland@banno.com>
2021-09-23 15:55:32 -0700

    Forward-port timestampBeforeNow

--- core/src/main/scala/com/banno/kafka/consumer/ConsumerOps.scala
+++ core/src/main/scala/com/banno/kafka/consumer/ConsumerOps.scala
@@ -51,6 +51,9 @@ object SeekTo {
   def committed(default: SeekTo): SeekTo = Committed(default)
   def offsets(offsets: Map[TopicPartition, Long], default: SeekTo): SeekTo =
     Offsets(offsets, default)
+  def timestampBeforeNow[F[_]: Clock: Functor](duration: FiniteDuration, default: SeekTo): F[SeekTo] =
+    Clock[F].realTime.map(now =>
+      timestamp(now.toMillis - duration.toMillis, default))
 
   def seek[F[_]: Monad](
       consumer: ConsumerApi[F, _, _],
