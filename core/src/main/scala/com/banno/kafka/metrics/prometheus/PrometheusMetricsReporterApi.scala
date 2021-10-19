a3a00ecb59b8194e6797e887d1b6d7fcdb9906bd
Zach Cox <zach.cox@banno.com>
2021-10-19 14:01:51 -0500

    define new consumer metrics

--- core/src/main/scala/com/banno/kafka/metrics/prometheus/PrometheusMetricsReporterApi.scala
+++ core/src/main/scala/com/banno/kafka/metrics/prometheus/PrometheusMetricsReporterApi.scala
@@ -827,6 +827,16 @@ object PrometheusMetricsReporterApi {
             ignore
           case MetricId("consumer-metrics", "last-poll-seconds-ago", List("client-id")) =>
             ignore
+          case MetricId("consumer-metrics", "idle-connection-close-total", List("client-id")) => ignore
+          case MetricId("consumer-metrics", "idle-connection-close-rate", List("client-id")) => ignore
+          case MetricId("consumer-metrics", "reverse-connection-added-total", List("client-id")) => ignore
+          case MetricId("consumer-metrics", "reverse-connection-added-rate", List("client-id")) => ignore
+          case MetricId("consumer-metrics", "reverse-connection-removed-total", List("client-id")) => ignore
+          case MetricId("consumer-metrics", "reverse-connection-removed-rate", List("client-id")) => ignore
+          case MetricId("consumer-metrics", "failed-handshake-total", List("client-id")) => ignore
+          case MetricId("consumer-metrics", "failed-handshake-rate", List("client-id")) => ignore
+          case MetricId("consumer-metrics", "handshake-time-ns-max", List("client-id")) => ignore
+          case MetricId("consumer-metrics", "handshake-time-ns-avg", List("client-id")) => ignore
           case MetricId(
               "consumer-coordinator-metrics",
               "partition-lost-latency-max",
