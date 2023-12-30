dea9175019e2483f8fe5ce55710fbe18cdb0ad76
Zach Cox <zcox522@gmail.com>
2023-12-30 11:22:00 -0700

    test commit after elapsed time

--- core/src/test/scala/com/banno/kafka/ProcessingAndCommittingSpec.scala
+++ core/src/test/scala/com/banno/kafka/ProcessingAndCommittingSpec.scala
@@ -65,7 +65,7 @@ class ProcessingAndCommittingSpec extends CatsEffectSuite with KafkaSpec {
               pac = consumer.processingAndCommitting(
                 pollTimeout = 100.millis,
                 maxRecordCount = 2,
-                maxCommitTime = Long.MaxValue,
+                maxElapsedTime = Long.MaxValue.nanos,
               )(_.value.pure[IO])
               committed = Stream.repeatEval(
                 consumer.partitionQueries.committed(ps)
@@ -104,8 +104,72 @@ class ProcessingAndCommittingSpec extends CatsEffectSuite with KafkaSpec {
       }
   }
 
+  // this has a real danger of becoming a "flaky test" due to its timing assumptions
   test("processingAndCommitting commits after elapsed time") {
-    // TODO
+    ProducerApi
+      .resource[IO, Int, Int](
+        BootstrapServers(bootstrapServer)
+      )
+      .use { producer =>
+        ConsumerApi
+          .resource[IO, Int, Int](
+            BootstrapServers(bootstrapServer),
+            GroupId(genGroupId),
+            AutoOffsetReset.earliest,
+            EnableAutoCommit(false),
+          )
+          .use { consumer =>
+            for {
+              topic <- createTestTopic[IO]()
+              p = new TopicPartition(topic, 0)
+              ps = Set(p)
+              values = (0 to 9).toList
+              _ <- producer.sendAsyncBatch(
+                values.map(v => new ProducerRecord(topic, v, v))
+              )
+              () <- consumer.subscribe(topic)
+              c0 <- consumer.partitionQueries.committed(ps)
+              // commit every 100 millis
+              pac = consumer.processingAndCommitting(
+                pollTimeout = 100.millis,
+                maxRecordCount = Long.MaxValue,
+                maxElapsedTime = 200.millis,
+              )(r => IO.sleep(101.millis).as(r.value))
+              committed = Stream.repeatEval(
+                consumer.partitionQueries.committed(ps)
+              )
+              results <- pac
+                .take(values.size.toLong)
+                .interleave(committed)
+                .compile
+                .toList
+            } yield {
+              assertEquals(c0, empty)
+              assertEquals(results.size, values.size * 2)
+              // TODO rewrite this to use values, not so hard-coded
+              assertEquals(results(0), 0)
+              assertEquals(results(1), empty)
+              assertEquals(results(2), 1)
+              assertEquals(results(3), offsets(p, 2))
+              assertEquals(results(4), 2)
+              assertEquals(results(5), offsets(p, 2))
+              assertEquals(results(6), 3)
+              assertEquals(results(7), offsets(p, 4))
+              assertEquals(results(8), 4)
+              assertEquals(results(9), offsets(p, 4))
+              assertEquals(results(10), 5)
+              assertEquals(results(11), offsets(p, 6))
+              assertEquals(results(12), 6)
+              assertEquals(results(13), offsets(p, 6))
+              assertEquals(results(14), 7)
+              assertEquals(results(15), offsets(p, 8))
+              assertEquals(results(16), 8)
+              assertEquals(results(17), offsets(p, 8))
+              assertEquals(results(18), 9)
+              assertEquals(results(19), offsets(p, 10))
+            }
+          }
+      }
   }
 
   case class CommitOnFailureException()
@@ -140,7 +204,7 @@ class ProcessingAndCommittingSpec extends CatsEffectSuite with KafkaSpec {
               pac = consumer.processingAndCommitting(
                 pollTimeout = 100.millis,
                 maxRecordCount = Long.MaxValue,
-                maxCommitTime = Long.MaxValue,
+                maxElapsedTime = Long.MaxValue.nanos,
               ) { r =>
                 val v = r.value
                 if (v == throwOn)
